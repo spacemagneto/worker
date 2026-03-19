5b618fd1ce481434e0a93567ccb669bc6d9af758
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-19 22:45:43 +0300

    default config

--- options.go
+++ options.go
@@ -1,7 +1,23 @@
 package worker
 
+import (
+	"runtime"
+)
+
+const DefaultMaxWorkerRestarts = 3
+
 type config struct {
 	workerCount       int
 	queueSize         int
 	maxWorkerRestarts int
+}
+
+func defaultConfig() config {
+	defWorkerCount := runtime.NumCPU()
+
+	return config{
+		workerCount:       defWorkerCount,
+		queueSize:         defWorkerCount * 2,
+		maxWorkerRestarts: DefaultMaxWorkerRestarts,
+	}
 }
