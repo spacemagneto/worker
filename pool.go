b5936c1f0b9a5894a1133c95ec50b61db7db06c0
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-03-19 22:59:06 +0300

    default pool constructor

--- pool.go
+++ pool.go
@@ -1,8 +1,37 @@
 package worker
 
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+)
+
 type Pool struct {
+	ctx               context.Context
+	contextCancelFunc context.CancelFunc
+
+	taskCh chan any
+
+	maxWorkerRestarts int64
+
+	isStop atomic.Bool
+	wg     sync.WaitGroup
 }
 
-func New() *Pool {
-	return &Pool{}
+func New(opts ...Option) *Pool {
+	cfg := defaultConfig()
+
+	for _, opt := range opts {
+		if opt != nil {
+			opt(&cfg)
+		}
+	}
+
+	ctx, cancel := context.WithCancel(cfg.context)
+	return &Pool{
+		ctx:               ctx,
+		contextCancelFunc: cancel,
+		taskCh:            make(chan any),
+		maxWorkerRestarts: int64(cfg.maxWorkerRestarts),
+	}
 }
