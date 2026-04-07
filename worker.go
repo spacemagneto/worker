ac28a7d5373cb6bbf040882ff4b8b0444f6a71b2
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-04-07 13:06:12 +0300

    worker struct and constructor

--- worker.go
+++ worker.go
@@ -1,1 +1,26 @@
 package worker
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+)
+
+type worker[T, R any] struct {
+	parentCtx             context.Context
+	id                    string
+	processingFunc        FuncWithResult[T, R]
+	jobCh                 <-chan job[T, R]
+	maxRetryWorkerRestart int32
+	restartCounter        atomic.Int32
+}
+
+func newWorker[T, R any](ctx context.Context, index int, processingFunc FuncWithResult[T, R], jobCh <-chan job[T, R], maxRetryWorkerRestart int32) *worker[T, R] {
+	return &worker[T, R]{
+		parentCtx:             ctx,
+		id:                    fmt.Sprintf("worker::%d", index),
+		processingFunc:        processingFunc,
+		jobCh:                 jobCh,
+		maxRetryWorkerRestart: maxRetryWorkerRestart,
+	}
+}
