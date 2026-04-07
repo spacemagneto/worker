a169ea1bb54e39ed392aebc81a86ea936b6b20c4
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-04-07 18:54:11 +0300

    worker

--- worker.go
+++ worker.go
@@ -3,6 +3,7 @@ package worker
 import (
 	"context"
 	"fmt"
+	"sync"
 	"sync/atomic"
 )
 
@@ -22,5 +23,80 @@ func newWorker[T, R any](ctx context.Context, index int, processingFunc FuncWith
 		processingFunc:        processingFunc,
 		jobCh:                 jobCh,
 		maxRetryWorkerRestart: maxRetryWorkerRestart,
+	}
+}
+
+func (w *worker[T, R]) start(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		tryRestart, err := w.workerLoop()
+		if !tryRestart {
+			return
+		}
+
+		if err != nil {
+			// TODO: and?
+		}
+
+		attempt := w.restartCounter.Add(1)
+
+		if attempt >= w.maxRetryWorkerRestart {
+			return
+		}
+	}
+}
+
+func (w *worker[T, R]) workerLoop() (isRestart bool, err error) {
+	defer func() {
+		if rec := recover(); rec != nil {
+			isRestart = true
+			err = GetRecoverError(rec)
+		}
+	}()
+
+	for {
+		select {
+		case <-w.parentCtx.Done():
+			// TODO: need close all result processing channels
+			return
+
+		case newJob, ok := <-w.jobCh:
+			if ok {
+				w.jobProcessing(newJob)
+			}
+
+			// TODO: Overall, we might be able to get out of this, but it's not a sure thing.
+		}
+	}
+}
+
+func (w *worker[T, R]) jobProcessing(j job[T, R]) {
+	processingCtx := w.parentCtx
+	var processingCtxCancel context.CancelFunc
+
+	if j.timeout > 0 {
+		processingCtx, processingCtxCancel = context.WithTimeout(w.parentCtx, j.timeout)
+		defer processingCtxCancel()
+	}
+
+	res, err := func() (result R, err error) {
+		defer func() {
+			if recoverErr := recover(); recoverErr != nil {
+				err = GetRecoverError(recoverErr)
+			}
+		}()
+
+		return w.processingFunc(processingCtx, j.data)
+	}()
+
+	if j.jobResult != nil {
+		j.jobResult.processingDone(res, err)
+	}
+
+	if err != nil {
+		if j.errorFunc != nil {
+			j.errorFunc(err)
+		}
 	}
 }
