2baf48721e475718f00936a66e2cb3b52066f3ca
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-04-08 23:32:08 +0300

    pool constructors

--- pool.go
+++ pool.go
@@ -39,6 +39,60 @@ type Pool[T, R any] struct {
 	logger *slog.Logger
 }
 
+func NewFunc[T any](processingFunc Func[T], opts ...Option) (*Pool[T, struct{}], error) {
+	if processingFunc == nil {
+		return nil, ErrProcessingFuncIsEmpty
+	}
+
+	return NewResult(func(ctx context.Context, input T) (struct{}, error) {
+		processingFunc(ctx, input)
+		return struct{}{}, nil
+	}, opts...)
+}
+
+func NewFuncWithError[T any](processingFunc FuncError[T], opts ...Option) (*Pool[T, struct{}], error) {
+	if processingFunc == nil {
+		return nil, ErrProcessingFuncIsEmpty
+	}
+
+	return NewResult(func(ctx context.Context, input T) (struct{}, error) {
+		return struct{}{}, processingFunc(ctx, input)
+	}, opts...)
+}
+
+func NewResult[T, R any](processingFunc FuncWithResult[T, R], opts ...Option) (*Pool[T, R], error) {
+	if processingFunc == nil {
+		return nil, ErrProcessingFuncIsEmpty
+	}
+
+	cfg := defaultConfig()
+
+	for _, opt := range opts {
+		if opt != nil {
+			opt(&cfg)
+		}
+	}
+
+	ctx, cancel := context.WithCancel(cfg.context)
+
+	pool := &Pool[T, R]{
+		ctx:                   ctx,
+		contextCancelFunc:     cancel,
+		workers:               cfg.workers,
+		jobQueue:              make(chan job[T, R], cfg.queueSize),
+		processingFunc:        processingFunc,
+		maxRetryWorkerRestart: int32(cfg.maxWorkerRestarts),
+		logger:                cfg.logger,
+	}
+
+	// I stole the idea after seeing a solution on GitHub for cleaning up resources once the main context had finished.
+	pool.parentContextHook = context.AfterFunc(ctx, func() {
+		pool.Stop()
+	})
+
+	return pool, nil
+}
+
 func (p *Pool[T, R]) Run() {
 	p.onceStart.Do(func() {
 		for index := range p.workers {
