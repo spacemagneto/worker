8d4b268990ebbdd708f2ec107db96abb543a35c7
syntaxErrorLineNULL <u5495527112@gmail.com>
2026-04-09 23:44:25 +0300

    use default value in test

--- options_test.go
+++ options_test.go
@@ -21,7 +21,7 @@ func TestPoolOptions(t *testing.T) {
 		assert.NotNil(t, cfg.context, "Expected default context to be initialized and not nil")
 		assert.Equal(t, runtime.NumCPU(), cfg.workers, "Expected default worker count to match number of CPUs")
 		assert.Equal(t, runtime.NumCPU()*2, cfg.queueSize, "Expected default queue size to be double worker count")
-		assert.Equal(t, 3, cfg.maxWorkerRestarts, "Expected default maximum worker restarts to be exactly 3")
+		assert.Equal(t, DefaultMaxWorkerRestarts, cfg.maxWorkerRestarts, "Expected default maximum worker restarts to be exactly 3")
 		assert.NotNil(t, cfg.logger, "Expected default logger to be initialized and not nil")
 	})
 
